629801c4bb8665a2647fa6516f891972ad8b2c6c
cposada23 <cposadaa@gmail.com>
2015-10-09 21:23:39 -0500

    primer commit

---
+++ Lab3ArqSoft-ejb/src/java/com/udea/session/FilmManager.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udea.session;
+
+import com.udea.entities.Film;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author cposa
+ */
+@Stateless
+@LocalBean
+public class FilmManager {
+    @PersistenceContext(unitName = "Lab3ArqSoft-ejbPU")
+    private EntityManager em;
+
+    public void persist(Object object) {
+        em.persist(object);
+    }
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+    public List<Film> getAllFilms() {
+        return em.createNamedQuery("Film.findAll").getResultList();
+    }
+
+    public Film update(Film film) {
+        return em.merge(film);
+    }
+    
+    
+}
---
+++ Lab3ArqSoft-war/src/java/mbeans/FilmMBean.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mbeans;
+
+import com.udea.entities.Film;
+import com.udea.session.FilmManager;
+import java.util.List;
+import javax.ejb.EJB;
+
+/**
+ *
+ * @author cposa
+ */
+public class FilmMBean {
+    @EJB
+    private FilmManager filmManager;
+
+    private Film film;
+    private List<Film> films;
+    
+    /**
+     * Creates a new instance of FilmMBean
+     */
+    public FilmMBean() {
+    }
+    public List<Film> getFilms(){
+        if(films==null|| films.isEmpty())refresh();
+        return films;
+    }
+    
+    public void refresh(){
+        films = filmManager.getAllFilms();
+    }
+    
+    public Film getDetails(){
+        return film;
+    }
+    
+    public String showDetails(Film film){
+        this.film= film;
+        return "FilmDetails";
+    }
+    
+    public String update(){
+        film = filmManager.update(film);
+        return "FilmList";
+    }
+    
+    public String list(){
+        return "FilmList";
+    }
+    
+}
