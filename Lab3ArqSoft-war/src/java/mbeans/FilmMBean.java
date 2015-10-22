a1747b257861cba1e2025bcb2df22de229de6c69
camilo.posadaa <camilo.posadaa@lis-sala3>
2015-10-22 11:58:55 -0500

    añade nuevo lenguaje y nueva pelicula con el idioma correcto, todavia falta corregir lo de la fecha y el delete en cascada.

--- Lab3ArqSoft-war/src/java/mbeans/FilmMBean.java
+++ Lab3ArqSoft-war/src/java/mbeans/FilmMBean.java
@@ -98,18 +98,8 @@ public class FilmMBean{
         refresh();
         Language la = laguageManager.getLanguageByName(film.getLanguageId().getName());
         
-        
-        System.out.println("Language buscado por nombre " + la.getName());
-        System.out.println("Id Language buscado por nombre" + la.getLanguageId());
-        
-        
         film.setLanguageId(la);
-        //debug---------------
-        System.out.println("update");
-        Language l = film.getLanguageId(); 
-        System.out.println("nombreLanguageAntesUpdate "+l.getName());
-        System.out.println("id Language antes "+l.getLanguageId());
-        //debug --------------
+        
         filmManager.persist(film);
         return "FilmList";
     }
